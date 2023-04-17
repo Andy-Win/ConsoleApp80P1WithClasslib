dfcbd2781862c25a128fdc86159cdfb4ccbfdce8
Andy Wang <v-wajian@microsoft.com>
2023-04-17 08:59:17 +0000

    Add project files.

---
+++ ConsoleApp80P1WithClasslib/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp80P1WithClasslib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary1.Class1 c = new ClassLibrary1.Class1();
+            Console.WriteLine("Hello from console app " + c.GetHello());
+            Console.ReadKey();
+        }
+    }
+}
